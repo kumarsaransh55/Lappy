a60d22b40b059dbf8e53aa24de03294186687809
Saransh <kumarsaransh55@gmail.com>
2026-01-14 21:38:59 +0530

    Added Order Controller & Views to view and modify existing orders

---
+++ LappyBag/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,59 @@
+﻿using Lappy.DataAccess.Repository.IRepository;
+using Lappy.Models;
+using Lappy.Utility;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LappyBag.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index(string status= "all")
+        {
+            ViewBag.Status = status;
+            return View();
+        }
+
+        public IActionResult Details(int orderId)
+        {
+            OrderVM orderDetailsViewModel = new()
+            {
+                orderHeader = _unitOfWork.OrderHeader.Get(u => u.Id == orderId, includeProperties: "ApplicationUser"),
+                orderDetails = _unitOfWork.OrderDetail.GetAll(u => u.OrderHeaderId == orderId, includeProperties: "Product")
+            };
+            return View(orderDetailsViewModel);
+        }
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll(string status)
+        {
+            List<OrderHeader> obj;
+            switch(status)
+            {
+                case "pending":
+                    obj = _unitOfWork.OrderHeader.GetAll(u => u.OrderStatus == SD.PaymentStatusDelayedPayment, includeProperties: "ApplicationUser").ToList();
+                    break;
+                case "inprocess":
+                    obj = _unitOfWork.OrderHeader.GetAll(u => u.OrderStatus == SD.StatusInProcess, includeProperties: "ApplicationUser").ToList();
+                    break;
+                case "completed":
+                    obj = _unitOfWork.OrderHeader.GetAll(u => u.OrderStatus == SD.StatusShipped, includeProperties: "ApplicationUser").ToList();
+                    break;
+                case "approved":
+                    obj = _unitOfWork.OrderHeader.GetAll(u => u.OrderStatus == SD.StatusApproved, includeProperties: "ApplicationUser").ToList();
+                    break;
+                default:
+                    obj = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+                    break;
+            }
+            return Json(new { data = obj });
+        }
+        #endregion
+    }
+}
--- LappyBag/wwwroot/js/order.js
+++ LappyBag/wwwroot/js/order.js
@@ -1,72 +1,29 @@
 ﻿
 $(document).ready(function () {
-    loadDataTable();
+    loadDataTable(status);
 });
+
 var dataTable;
-function loadDataTable() {
+function loadDataTable(status) {
+    console.log(status);
     dataTable = $('#tblData').DataTable({
-        "ajax": { url: '/Admin/Product/GetAll/' },
+        "ajax": { url: '/Admin/Order/GetAll?status='+ status},
         "columns": [
-            { data: 'brand', "width": "5%" },
-            { data: 'modelName', "width": "15%" },
-            { data: 'screenSize', "width": "10%" },
-            { data: 'cpuModel', "width": "20%" },
-            { data: 'ramMemory', "width": "5%" },
-            { data: 'hardDiskSize', "width": "10%" },
-            { data: 'category.name', "width": "10%" },
-            { data: 'os', "width": "15%" },
+            { data: 'id', "width": "5%" },
+            { data: 'name', "width": "15%" },
+            { data: 'phoneNumber', "width": "15%" },
+            { data: 'applicationUser.email', "width": "20%" },
+            { data: 'orderStatus', "width": "5%" },
+            { data: 'orderTotal', "width": "10%" },
             {
                 data: 'id',
                 "render": function (data) {
                     return  ` <div class="d-flex gap-2">
-                                   <a href= "Product/Upsert/${data}" class="btn btn-primary">Edit</a>
-                                   <a Onclick="deleteProduct(${data})" class="btn btn-danger">Delete</a>
+                                   <a href= "order/details?orderId=${data}" class="btn btn-primary">Edit</a>
                              </div>`
                 },
-                "width": "15%" 
+                "width": "10%" 
             }
         ]
-    });
-}
-function deleteProduct(id){
-    Swal.fire({
-        title: "Are you sure?",
-        text: "You won't be able to revert this!",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "Yes, delete it!"
-    }).then((result) => {
-        if (result.isConfirmed) {
-            $.ajax({
-                type: "DELETE",
-                url: `/Admin/Product/Delete?id=${ id }`,
-                success: function (data) {
-                    if (data.success) {
-                        dataTable.ajax.reload();
-                        Swal.fire({
-                            title: "Deleted!",
-                            text: "Your Product has been deleted.",
-                            icon: "success"
-                        });
-                    } else {
-                        Swal.fire({
-                            title: "Not Deleted!",
-                            text: "Your Product has not been deleted.",
-                            icon: "warning"
-                        });
-                    }
-                    
-                },
-                error: function (data) {
-                    Swal.fire({
-                        title: "Error!",
-                        text: "Error encountered while deleting the product.",
-                        icon: "error"
-                    });
-                }
-            });
-        }
     });
 }
